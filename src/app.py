4c03f8dc81e6378b2fa12cfc9fbc8c757feda5a0
giovannabbottino <giovannabbottino@gmail.com>
2022-12-04 17:41:23 -0300

    adicionaa tratamento de excecao
    Co-authored-by: Felipe Agustini <felipeboccardi@hotmail.com>

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from controller.file import FileController
 from controller.graph import GraphController
 
@@ -18,29 +18,38 @@ app = Flask(__name__)
 
 @app.route('/caminho', methods=["POST"])
 def caminho():
-    start_node = str(request.form.get('start'))
-    target_node = str(request.form.get('target'))
+    try:
+        start_node = str(request.form.get('start'))
+        target_node = str(request.form.get('target'))
 
-    previous_nodes, _ = controller.graph.dijkstra_algorithm(start_node=start_node) 
-    path = controller.get_path(previous_nodes, start_node=start_node, target_node=target_node)
+        previous_nodes, _ = controller.graph.dijkstra_algorithm(start_node=start_node) 
+        path = controller.get_path(previous_nodes, start_node=start_node, target_node=target_node)
 
-    for filename in os.listdir('./static/images/'):
-        if filename.startswith('path_'):  
-            os.remove('./static/images/' + filename)
+        for filename in os.listdir('./static/images/'):
+            if filename.startswith('path_'):  
+                os.remove(f'./static/images/{filename}')
 
-    position = []
-    for local in path:
-        position.append(positions[local])
+        image = cv2.imread('./static/images/mapa-beto-carrero-world.png')
 
-    image = cv2.imread('./static/images/mapa-beto-carrero-world.png')
+        position = []
+        for local in path:
+            position.append(positions[local])
 
-    for i in position:
-        cv2.line(image, i[0], i[1], (0,0,255), 2)
+        for i in range(0, len(position)- 1):
+            cv2.line(image, (position[i][0], position[i][1]), (position[i + 1][0], position[i+1][1]), (255, 255, 255), 5)
 
-    new_image_name = "path_" + start_node + '_' +  target_node + ".png"
-    cv2.imwrite("./static/images/" + new_image_name, image)
+        new_image_name = f'path_{start_node}_{target_node}.png'
+        cv2.imwrite(f'./static/images/{new_image_name}', image)
 
-    return render_template('base.html',nodes=nodes, path=path, image= "images/" + new_image_name)
+        return render_template(
+            'caminho.html',
+            start_node=start_node, 
+            target_node=target_node, 
+            nodes=nodes, 
+            path=path, 
+            image=f'images/{new_image_name}')
+    except:
+        return redirect(url_for('/'))
 
 @app.route('/', methods=["GET"])
 def index():
